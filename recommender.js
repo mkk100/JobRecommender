b5daf7059acd249e449cb431e7f19496c3a7991f
mkk100 <>
2023-03-11 16:08:30 -0800

    skeleton

--- recommender.js
+++ recommender.js
@@ -1,8 +1,6 @@
-const apiKey = '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';
+const apiKey = '';
 const apiUrl = 'https://api.openai.com/v1/completions';
-
-const promptInput = document.getElementById('prompt');
-const submitBtn = document.getElementById('submit-btn');
+const submitBtn = document.getElementById('btn');
 const outputDiv = document.getElementById('output');
 
 submitBtn.addEventListener('click', async () => {
@@ -10,23 +8,28 @@ submitBtn.addEventListener('click', async () => {
         headers: {
             Authorization: "Bearer " + apiKey,
         },
-        });
-    const prompt = promptInput.value
-    if (!prompt) {
-        outputDiv.textContent = 'Please enter a prompt.';
-        return;
+    });
+    const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+    prompt = "Find me the job where I can use my ";
+    // Loop through the selected checkboxes
+    for (let i = 1; i < checkboxes.length; i++) {
+        // Do something with the selected checkbox, for example, log its value
+        prompt += checkboxes[i].value;
+        prompt += ", "
     }
 
     try {
         const result = await response.post(apiUrl, {
             prompt: prompt,
             model: "text-davinci-003",
-            max_tokens: 300,
-            temperature: 0.5,
+            max_tokens: 500,
+            temperature: 0.2,
         });
-
         const outputText = result.data.choices[0].text;
-        outputDiv.textContent = outputText;
+        console.log(outputText)
+        localStorage.setItem('value', outputText);
+        window.location.href = 'recommender.html';
+
     } catch (error) {
         outputDiv.textContent = `Error: ${error.message}`;
     }
